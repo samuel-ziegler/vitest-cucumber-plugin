05c4b109e1e8b35e632b419aae53fc5e6174da00
Sam Ziegler <sam@ziegler.org>
2023-04-28 08:46:30 -0700

    Provide code snippets when a step is not defined.  Fixes #21.

--- src/steps.js
+++ src/steps.js
@@ -4,6 +4,12 @@ import { log } from './logger.js';
 
 var steps = [];
 
+const typeName = {
+    given : 'Given',
+    then : 'Then',
+    when : 'When',
+};
+
 const expressionFactory = new ExpressionFactory(new ParameterTypeRegistry());
 
 export const addStepDefinition = (expression,f) => {
@@ -31,7 +37,14 @@ export const findStepDefinitionMatch = (step) => {
     const stepDefinitionMatches = findStepDefinitionMatches(step);
     
     if (!stepDefinitionMatches || (stepDefinitionMatches.length == 0)) {
-        throw new Error('Step not defined: \''+step.type.name+' '+step.text+'\'');
+        throw new Error(`Undefined.  Implement with the following snippet:
+
+    ${typeName[step.type.type]}('${step.text}', (state,params,data) => {
+        // Write code here that turns the phrase above into concrete actions
+        throw new Error('Not yet implemented!');
+        return state;
+    });
+`);
     }
 
     if (stepDefinitionMatches.length > 1) {
